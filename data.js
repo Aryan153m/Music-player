2f5c2ff9e8ba97923d5b53731fc3da0a1c8fffed
Aryan Makwana <aryan018m@gmail.com>
2023-08-30 12:49:38 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,53 @@
+let songs = [
+    {
+        name: 'ram siya ram',
+        path: 'music/Ram Siya Ram.mp3',
+        artist: 'Sachet',
+        cover: 'images/ram.jpeg',
+    },
+    {
+        name: 'its magic ',
+        path: 'music/magic.mp3',
+        artist: 'Taz - Stereo Nation',
+        cover: 'images/jadoo.png',
+    },
+    {
+        name: 'Aasman ko chuke dekho',
+        path: 'music/Aasman ko chuke dekho.mp3',
+        artist: 'Delar ',
+        cover: 'images/Hanuman.jpg',
+    },
+    {
+        name: 'Mujhse Shaadi Karogi',
+        path: 'music/Mujhse Shaadi Karogi.mp3',
+        artist: 'Sonu Nigam',
+        cover: 'images/shaadi.jpg',
+    },
+    {
+        name: 'Insane',
+        path: 'music/Insane.mp3',
+        artist: 'AP Dhillon',
+        cover: 'images/insane.jpeg',
+    },
+    {
+        name: 'Just chill',
+        path: 'music/Just-Chill-Chill.mp3',
+        artist: 'sonu nigam',
+        cover: '',
+    },
+    {
+        name: 'Heat-Waves',
+        path: 'music/Heat-Waves.mp3',
+        artist: 'Glass Animals',
+        cover: '',
+    },
+    {
+        name: 'Mockingbird',
+        path: 'music/mockingbird.mp3',
+        artist: 'Eminem',
+        cover: '',
+    },
+    
+
+    
+]
